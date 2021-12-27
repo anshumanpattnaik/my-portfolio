2859cfd7e667995a7e200076e02533bfb523a25f
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2021-12-28 01:04:50 +0530

    Technical Skills Design

--- app/views.py
+++ app/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.shortcuts import render
 
 from app.models import TechPlatforms, TechStack
@@ -9,13 +7,10 @@ def index(request):
     tech_platforms = TechPlatforms.objects.all()
     tech_skills = []
     for _index, platform in enumerate(tech_platforms, start=1):
-        domains = []
         tech_stack = TechStack.objects.filter(domain_id=_index)
-        for tech in tech_stack:
-            domains.append(tech.domain)
         result = {
             "platform": platform.platform,
-            "domains": domains
+            "tech_stack": tech_stack
         }
         tech_skills.append(result)
     context = {
