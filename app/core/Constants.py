26902e7b6d2416e3cc90004347df5191cd337d0a
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2022-01-04 01:36:44 +0530

    Navigation animation

--- app/core/Constants.py
+++ app/core/Constants.py
@@ -1,10 +1,10 @@
 class Constants:
     NAV_MENU = '<nav>' \
                '<ul>' \
-               '<li><a href="/">Home</a></li>' \
-               '<li><a href="#technical-skills">Technical Skills</a></li>' \
-               '<li><a href="#projects">Projects</a></li>' \
-               '<li><a href="#blogs">Blogs</a></li>' \
-               '<li><a href="#about-me">About Me</a></li>' \
+               '<li><a href="http://192.168.29.113:8000/#home">Home</a></li>' \
+               '<li><a href="http://192.168.29.113:8000/#technical-skills">Technical Skills</a></li>' \
+               '<li><a href="http://192.168.29.113:8000/#projects">Projects</a></li>' \
+               '<li><a href="http://192.168.29.113:8000/#blogs">Blogs</a></li>' \
+               '<li><a href="http://192.168.29.113:8000/#about-me">About Me</a></li>' \
                '</ul>' \
                '</nav>'
