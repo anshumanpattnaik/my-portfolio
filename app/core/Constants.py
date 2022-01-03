abfdcfd86ca5d34ef03f378a99709ef59227a9c7
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2022-01-04 00:40:42 +0530

    Nav bar animation

--- app/core/Constants.py
+++ app/core/Constants.py
@@ -2,9 +2,9 @@ class Constants:
     NAV_MENU = '<nav>' \
                '<ul>' \
                '<li><a href="/">Home</a></li>' \
-               '<li><a href="#">Technical Skills</a></li>' \
-               '<li><a href="/projects">Projects</a></li>' \
-               '<li><a href="#">Blogs</a></li>' \
-               '<li><a href="#">About Me</a></li>' \
+               '<li><a href="#technical-skills">Technical Skills</a></li>' \
+               '<li><a href="#projects">Projects</a></li>' \
+               '<li><a href="#blogs">Blogs</a></li>' \
+               '<li><a href="#about-me">About Me</a></li>' \
                '</ul>' \
                '</nav>'
