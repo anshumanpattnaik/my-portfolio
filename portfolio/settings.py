3ba489a782336bc9cc626c759ee3fd75488c29f5
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2022-01-14 00:36:24 +0530

    Build and Deploy

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env.dev'))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
