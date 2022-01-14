fcdea1805d5e2455809d8a50127f8a98273f12c7
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2022-01-15 00:33:55 +0530

    migrations shell script update

--- build/migrations.sh
+++ build/migrations.sh
@@ -1,6 +1,6 @@
-python3 manage.py collectstatic
+python3 manage.py collectstatic --no-input
 python3 manage.py flush --no-input
 python3 manage.py migrate
 python3 manage.py makemigrations app
 python3 manage.py migrate app
-python3 admin.py
+# python3 admin.py
