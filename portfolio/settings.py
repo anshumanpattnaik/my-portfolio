9459175bbfb66751c052e12584ad5c3d5dcdbb84
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2022-01-14 20:21:18 +0530

    Deployment issue fixes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (bool(int(os.environ.get('DEBUG'))))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['anshuman.me']
 
 
 # Application definition
